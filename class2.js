30c24eb5b7267c7d5104c23afa5491dd1ead299b
itsadityae <adityaedq@gmail.com>
2025-03-02 11:02:41 +0530

    Initial commit

--- class2.js
+++ class2.js
@@ -1,16 +1,16 @@
 //Class : 2 (not complete)
 
 const person6 = {
-    firstname : "krish",
-    lastname : "raju",
+    firstname : "Nidhi",
+    lastname : "vasireddy",
     fullname : function(){
         console.log (${this.firstname} ${this.lastname});
     }
 }
 
 const person7 = {
-    firstname : "vatsal",
-    lastname : "singh",
+    firstname : "Aditya",
+    lastname : "Eslavath",
 
     // how to access the person6 function in this object?
     fullname  : person6.fullname(),
